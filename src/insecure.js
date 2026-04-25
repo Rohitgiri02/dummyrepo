6f697d512c86d4292aba2bed562d59bf87d4ae03
Rohit giri <rohitgiri0204@gmail.com>
2026-04-25 09:43:53 +0530

    added vulnerabilty

--- src/insecure.js
+++ src/insecure.js
@@ -1,3 +1,8 @@
 // Intentionally insecure sample file for testing analyzer behavior.
 // Do not use this code in production.
 //i am insecure
+function buildQuery(userId) {
+  // SQL concatenation should be flagged.
+  const query = "SELECT * FROM users WHERE id = '" + userId + "'";
+  return query;
+}
