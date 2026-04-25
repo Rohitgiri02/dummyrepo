6804d2f76eec55836258bfd6ed5ba9e66f95a76b
Rohit giri <rohitgiri0204@gmail.com>
2026-04-25 08:58:39 +0530

    removed vulnerability

--- src/insecure.js
+++ src/insecure.js
@@ -1,20 +1,3 @@
 // Intentionally insecure sample file for testing analyzer behavior.
 // Do not use this code in production.
 //i am insecure
-function runDynamic(userInput) {
-  // eval usage should be flagged.
-  return eval(userInput);
-}
-
-function buildQuery(userId) {
-  // SQL concatenation should be flagged.
-  const query = "SELECT * FROM users WHERE id = '" + userId + "'";
-  return query;
-}
-
-const hardcodedSecret = "sk_test_very_insecure_hardcoded_value";
-module.exports = {
-  runDynamic,
-  buildQuery,
-  hardcodedSecret,
-};
